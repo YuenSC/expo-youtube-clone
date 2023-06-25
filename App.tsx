b681871dd71ab0f14d3db4fc3c0e25b2154bbd46
Calvin Yuen <scyuenab@gmail.com>
2023-06-25 14:24:43 +0800

    feat: Build android app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
-setTimeout(SplashScreen.hideAsync, 5000);
-
 export default function App() {
   return (
     <View style={styles.container}>
